4e03d13893820c44ce5b61dbe70228268e6e7095
Divyansh Shivashok <divyanshshivashok@gmail.com>
2022-11-11 22:59:05 -0500

    initial db gen

---
+++ datasetMaker.py
@@ -0,0 +1,52 @@
+import argparse
+import copy
+import itertools
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--device", type=int, default=0)
+    parser.add_argument("--width", help='cap width', type=int, default=960)
+    parser.add_argument("--height", help='cap height', type=int, default=540)
+
+    parser.add_argument('--use_static_image_mode', action='store_true')
+    parser.add_argument("--min_detection_confidence",
+                        help='min_detection_confidence',
+                        type=float,
+                        default=0.7)
+    parser.add_argument("--min_tracking_confidence",
+                        help='min_tracking_confidence',
+                        type=int,
+                        default=0.5)
+
+    args = parser.parse_args()
+
+    return args
+
+'''
+    1. Prep camera
+    2. Load hand landmark model
+    3. Open camera and detect mp hand
+    4. Calculate landmarks
+    5. Normalize coordinates
+    6. Logging into csv file
+    7. Draw detections
+'''
+
+def main():
+    args = get_args()
+
+    cap_device = args.device
+    cap_width = args.width
+    cap_height = args.height
+
+    use_static_image_mode = args.use_static_image_mode
+    min_detection_confidence = args.min_detection_confidence
+    min_tracking_confidence = args.min_tracking_confidence
+
+    
